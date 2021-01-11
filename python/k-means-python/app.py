93d55365c19dccef9c52beeb0c2ce4388fc3c1e7
Patrick McCarver <patrick@merklebros.com>
2021-01-11 00:18:12 -0500

    app.py: k-means for cluster range, find knee point
    Previously __main__ fit k-means using a hard-coded number of clusters.
    Instead, we can fit k-means for a range of cluster values and
    pick the "best" one. The "elbow method" is one way of finding this
    "best" value. The kneed library attempts to find this elbow. Providing the
    inertia (sum of square error from centroid for all pixels) for each value of
    number of clusters allows kneed to find an optimal elbow.
    
    fit_k_means_for_range performs k means clustering over a range of
    cluster values
    
    get_numbers_of_clusters_and_inertias gets inertias for each value of
    number of clusters
    
    get_knee_point finds the "elbow" given x and y data
    
    plot_line_chart plots a line chart, useful for manually inspecting
    whether the calculated "elbow" matches where we would expect to see it

--- python/k-means-python/app.py
+++ python/k-means-python/app.py
@@ -5,10 +5,12 @@ Functions for k-means color clustering on images
 
 from os import listdir
 from pathlib import Path
+from typing import List, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from sklearn.cluster import KMeans
+from kneed import KneeLocator
 
 IMAGES_DIRECTORY = "./../../data/images/"
 PLOTS_DIRECTORY = "../../data/plots"
@@ -39,6 +41,68 @@ def fit_k_means(data, number_of_clusters):
     k_means.fit(data)
 
     return k_means
+
+
+def fit_k_means_for_range(data, start: int, stop: int) -> list:
+    """Perform k_means clustering for a range of cluster values
+
+    :data: array-like data for k-means clustering
+    :start: starting value for range (int)
+    :stop: ending value for range (int)
+    :returns: List of tuples of (fitted k_means object, number of clusters)
+        of type [(sklearn.cluster.KMeans, int)]
+
+    """
+    k_means = []
+    for i in range(start, stop + 1):
+        k_means.append((fit_k_means(data, i), i))
+    return k_means
+
+
+def get_numbers_of_clusters_and_inertias(
+    k_means_list: list,
+) -> Tuple[List[int], List[int]]:
+    """Return tuple of ([numbers of clusters], [k-means fitted inertias])
+
+    :k_means_list: List of tuples of (fitted k_means object, number of clusters):
+        [(sklearn.cluster.KMeans, int)]
+    :returns: Tuple of (numbers of clusters, inertias)
+
+    """
+    x = []
+    y = []
+    for k_means, number_of_clusters in k_means_list:
+        x.append(number_of_clusters)
+        y.append(k_means.inertia_)
+    return (x, y)
+
+
+def get_knee_point(
+    x: List[int], y: List[int], sensitivity: float, curvature: str, slope: str
+) -> int:
+    """Find the knee point for y vs x
+
+    :x: List of numeric x values
+    :y: List of numeric y values
+    :sensitivity: sensitivity parameter for kneed
+    :curvature: one of "convex" or "concave"
+    :slope: one of "increasing" or "decreasing"
+    :returns: The x value where the knee point is located
+
+    """
+    kneedle = KneeLocator(x, y, S=sensitivity, curve=curvature, direction=slope)
+    return kneedle.elbow
+
+
+def plot_line_chart(x: List[int], y: List[int], file_name):
+    """Save a line chart
+    :x: Plot x values [int]
+    :y: Plot y values [int]
+    :file_name: Path to save line chart str
+
+    """
+    plt.plot(x, y)
+    plt.savefig(file_name)
 
 
 def plot_k_means_pie_chart(k_means, file_name):
@@ -78,10 +142,27 @@ def main(directory, plot_directory):
     """
     paths = listdir(directory)
     for path in paths:
-        image = load_image(path=f"{IMAGES_DIRECTORY}{path}")
-        # TODO: Currently hardcoded k, save k for each image in json somewhere
-        k_means = fit_k_means(image, 8)
         file_stem = Path(path).stem
+        image = load_image(path=f"{IMAGES_DIRECTORY}{path}")
+        k_means_list = fit_k_means_for_range(image, 1, 10)
+        print(k_means_list)
+        numbers_of_clusters, inertias = get_numbers_of_clusters_and_inertias(
+            k_means_list
+        )
+        plot_line_chart(
+            numbers_of_clusters,
+            inertias,
+            file_name=f"{plot_directory}/{file_stem}-line.png",
+        )
+        knee = get_knee_point(
+            x=numbers_of_clusters,
+            y=inertias,
+            sensitivity=5.0,
+            curvature="convex",
+            slope="decreasing",
+        )
+        print(knee)
+        k_means = k_means_list[knee - 1 + 2][0]
         plot_k_means_pie_chart(
             k_means, file_name=f"{plot_directory}/{file_stem}-pie.png"
         )
